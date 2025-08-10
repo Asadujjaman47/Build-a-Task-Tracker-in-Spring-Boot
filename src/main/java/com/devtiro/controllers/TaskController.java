8fab935015d9a4e8c16f0a2fdba25420fcb0cb2c
Asad <asad1@itconquest.com>
2025-08-10 15:52:28 +0600

    Update Task

--- src/main/java/com/devtiro/controllers/TaskController.java
+++ src/main/java/com/devtiro/controllers/TaskController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -89,6 +90,25 @@ public class TaskController {
                 .map(taskMapper::toDto)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{task_id}")
+    @Operation(summary = "Update a specific task", description = "Updates details of a specific task in the task list")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Task updated successfully",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = TaskDto.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid input data"),
+            @ApiResponse(responseCode = "404", description = "Task not found")
+    })
+    public ResponseEntity<TaskDto> updateTask(
+            @PathVariable("task_list_id") UUID taskListId,
+            @PathVariable("task_id") UUID taskId,
+            @RequestBody TaskDto taskDto) {
+        TaskDto updatedTask = taskMapper.toDto(
+                taskService.updateTask(taskListId, taskId, taskMapper.fromDto(taskDto))
+        );
+        return ResponseEntity.ok(updatedTask);
     }
 
 }
