32fb3562146364ec144cb7928a3b796f10cf30b2
Asad <asad1@itconquest.com>
2025-08-10 11:07:26 +0600

    Create Task List Endpoint

--- src/main/java/com/devtiro/controllers/TaskListController.java
+++ src/main/java/com/devtiro/controllers/TaskListController.java
@@ -1,9 +1,12 @@
 package com.devtiro.controllers;
 
 import com.devtiro.domain.dto.TaskListDto;
+import com.devtiro.domain.entities.TaskList;
 import com.devtiro.mapper.TaskListMapper;
 import com.devtiro.services.TaskListService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +30,13 @@ public class TaskListController {
                 .stream()
                 .map(taskListMapper::toDto)
                 .toList();
+    }
+
+    @PostMapping
+    public TaskListDto createTaskList(@RequestBody TaskListDto taskListDto) {
+        TaskList createdTaskList = taskListService.createTaskLists(
+                taskListMapper.fromDto(taskListDto)
+        );
+        return taskListMapper.toDto(createdTaskList);
     }
 }
